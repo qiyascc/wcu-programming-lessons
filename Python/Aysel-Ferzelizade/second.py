7070343edc2a7c630d7ea48951d5126b1acdce07
Qiyas <107604130+qiyascc@users.noreply.github.com>
2023-09-21 12:36:47 +0400

    Update second.py

--- Python/Aysel-Ferzelizade/second.py
+++ Python/Aysel-Ferzelizade/second.py
@@ -1,14 +1,40 @@
+######################################################################################
 # Single line comment.
 
 '''
 Multiine comment.
 '''
-
+#######################################################################################
 name = "Qiyas"
 # print("my name is ", name)
-
+#######################################################################################
 question = "Enter your name: "
 # print(question)
-name = input()
+# username = input() # type . for builtin functions. 
+# EXAMPLE
+# username = input().capitalize()
+# username = input().isalnum()
+# username = input().upper()
 welcome_message = "Welcome"
-# print(welcome_message, name)
+# print(welcome_message, username)
+########################################################################################
+
+pin = "501"
+question = "Enter your card password: "
+print(question)
+card_number = input()
+card_number_is_digit = card_number.isdigit()
+if card_number_is_digit == True:
+    pin_question = "Enter your card pin: "
+    print(pin_question)
+    pin_from_user = input()
+    if pin_from_user == pin:
+        print("Ur card password: ", card_number, "\nur pin: ", pin_from_user)
+    else:
+        print("U re scammer.")
+else: 
+    print("Card password is not alphabetic.")
+
+#########################################################################################
+
+  
