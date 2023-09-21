6946bf65a74bbfd364c7afb62697e91a64992fd1
Qiyas <107604130+qiyascc@users.noreply.github.com>
2023-09-21 12:10:09 +0400

    Second.py

---
+++ Python/Aysel-Ferzelizade/second.py
@@ -0,0 +1,14 @@
+# Single line comment.
+
+'''
+Multiine comment.
+'''
+
+name = "Qiyas"
+# print("my name is ", name)
+
+question = "Enter your name: "
+# print(question)
+name = input()
+welcome_message = "Welcome"
+# print(welcome_message, name)
