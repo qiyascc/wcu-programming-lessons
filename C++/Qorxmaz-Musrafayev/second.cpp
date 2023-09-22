542f56823697a09ab0a779f01686ed4d21de0772
Qiyas <107604130+qiyascc@users.noreply.github.com>
2023-09-22 12:39:04 +0400

    Create second.cpp

---
+++ C++/Qorxmaz-Musrafayev/second.cpp
@@ -0,0 +1,38 @@
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//   int a = 8, b = 5;
+//   cout<<(a > b)<<endl;
+
+//   return 0;
+// }
+
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//   int a = 8, b = 5, c = 4;
+//   cout<<(a>b && b>c)<<endl;
+//   return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//   int a,numbers,number_x, result;
+//   cout << "Enter: ";
+//   cin >> a;
+//   numbers = a/10;
+//   number_x = a%10;
+//   result = numbers + number_x;
+
+//   cout << "Answer: " << result;    
+    
+//   return 0;
+// }
+
+
+
