4f883363a54f08c9d75283eabc1e699e194bf2a2
Qiyas <107604130+qiyascc@users.noreply.github.com>
2023-09-22 11:09:14 +0400

    Update first.cpp

--- C++/Qorxmaz-Musrafayev/first.cpp
+++ C++/Qorxmaz-Musrafayev/first.cpp
@@ -1,1 +1,12 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int main() {
+//     cout<<"Hello World";
+
+//     return 0;
+}
+
+// <-- This is a comment line character. Select and use ctrl+/ 
 
