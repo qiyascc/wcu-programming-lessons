2340de4e02aec2d812cff925c5b83f5f13e914b1
Qiyas <107604130+qiyascc@users.noreply.github.com>
2023-11-02 12:34:57 +0400

    Create third.py

---
+++ Python/Lale Mustafayeva/lesson 8/third.py
@@ -0,0 +1,6 @@
+def print_odd_numbers(x):
+    for num in range(1, x + 1):
+        if num % 2 != 0: 
+            print(num)
+
+print_odd_numbers(8) # Verilən ədədə (parametrə) qədər olan tək ədədləri tapmaq üçün.
